8c70ab8bec3afec643dbaff1381a9fe556809795
Igor Prus <prus.igor@gmail.com>
2019-01-23 13:05:24 +0300

    added data jpa. added repository

---
+++ src/main/java/com/prus/library/repository/AuthorReposiroryJpa.java
@@ -0,0 +1,50 @@
+package com.prus.library.repository;
+
+import com.prus.library.domain.Author;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AuthorReposiroryJpa implements AuthorRepository{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Author getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Author> getByFirstName(String firstName) {
+        return null;
+    }
+
+    @Override
+    public List<Author> getByLastName(String lastName) {
+        return null;
+    }
+
+    @Override
+    public long insert(Author author) {
+        em.persist(author);
+    }
+
+    @Override
+    public List<Author> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean existInDatabase(Author author) {
+        return false;
+    }
+
+    @Override
+    public Author getByFullName(String f, String l) {
+        return null;
+    }
+}
---
+++ src/main/java/com/prus/library/repository/AuthorRepository.java
@@ -0,0 +1,22 @@
+package com.prus.library.repository;
+
+import com.prus.library.domain.Author;
+
+import java.util.List;
+
+public interface AuthorRepository {
+
+    Author getById(long id);
+
+    List<Author> getByFirstName(String firstName);
+
+    List<Author> getByLastName(String lastName);
+
+    long insert(Author author);
+
+    List<Author> getAll();
+
+    boolean existInDatabase(Author author);
+
+    Author getByFullName(String f, String l);
+}
